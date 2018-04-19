c12d22a94fea62cec1163044fb204b6aba32b0f7
jinhyeokfang <jinhyeokfang@gmail.com>
2018-04-19 09:41:23 +0900

    remove view

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const klunch = require('k-lunch')
 
 const app = express()
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
-app.use(express.static(path.join(__dirname, 'public')))
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('index')
